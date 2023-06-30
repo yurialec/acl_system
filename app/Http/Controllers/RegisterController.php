3e108e3eec3cfa7482b5511127fe01d9efd4da4a
Yuri Alec <yurialec@hotmail.com>
2023-06-30 11:46:58 -0300

    create prject, setup database credentials, setup model, setup registration controller, setup registration request

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    /**
+     * Display register page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        return view('auth.register');
+    }
+
+
+    /**
+     * Handle account registration request
+     *
+     * @param RegisterRequest $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create($request->validated());
+        auth()->login($user);
+
+        return redirect('/')->with('sucess', "Account successfully registered.");
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'username',
         'password',
     ];
 
@@ -42,4 +43,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
 }
